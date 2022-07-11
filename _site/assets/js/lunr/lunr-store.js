dde5a28f3a872ce84c6d7de675991192ec0f4dfe
Steffen Karcher <steffen.karcher@googlemail.com>
2022-07-11 18:26:18 +0200

    new build

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Adding multiple FlutterViews to Android app",
-        "excerpt":"[WIP:] Adding multiple FlutterViews to Android app Introduction Last month, as a mobile developer at Nutrilize, I had to find a way to combine our iOS and Android app in order to simplify and speed up the development of new features in the future. In my opinion, Flutter clearly is...","categories": [],
+        "title": "[WIP:] Adding multiple FlutterViews to Android app",
+        "excerpt":"Introduction Last month, as a mobile developer at Nutrilize, I had to find a way to combine our iOS and Android app in order to simplify and speed up the development of new features in the future. In my opinion, Flutter clearly is the best option to accomplish this task...","categories": [],
         "tags": [],
         "url": "/2022/07/07/adding-multiple-flutter-views-to-android-app.html",
         "teaser": null
